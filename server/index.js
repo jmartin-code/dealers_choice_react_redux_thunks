7a2b1d4216543301fc144758f60a05314ced3c47
jmartin-code <jmmartinez331@gmail.com>
2021-09-05 19:16:59 -0400

    working on the database

--- server/index.js
+++ server/index.js
@@ -15,15 +15,19 @@ app.get('/api/violations', async (req, res, next) => {
 
 })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`server running on port ${port}`));
-
 
 const { Sequelize, STRING } = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:fullstack25@localhost/ecb_violations');
 
 //Models
-const Violaton = db.define('violation', {
+const Building = db.define('building', {
+    bin: {
+        type: STRING,
+    }
+
+})
+
+const Violation = db.define('violation', {
     isn_dob_bis_extract: STRING,
     ecb_violation_number: STRING,
     ecb_violation_status: STRING,
@@ -52,4 +56,37 @@ const Violaton = db.define('violation', {
     aggravated_level: STRING,
     hearing_status: STRING,
     certification_status: STRING
-})
+});
+
+//Associations
+Violation.belongsTo(Building);
+Building.hasMany(Violation);
+
+const syncAndSeed = async () => {
+    try {
+        await db.authenticate();
+        await db.sync({ force: true })
+        console.log('Connected to database');
+
+        await Building.create({ bin: "1014398" });
+        await Promise.all(data.map(violation => Violation.create(violation)));
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+const runServer = async () => {
+    try {
+        await syncAndSeed();
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`server running on port ${port}`));
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+runServer();
+
+
