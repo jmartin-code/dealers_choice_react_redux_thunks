87bb8f1d48b9c5cf2e0039e7b754128e0820b3b2
jmartin-code <jmmartinez331@gmail.com>
2021-09-04 23:43:48 -0400

    adding db model

--- server/index.js
+++ server/index.js
@@ -21,3 +21,35 @@ app.listen(port, () => console.log(`server running on port ${port}`));
 
 const { Sequelize, STRING } = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:fullstack25@localhost/ecb_violations');
+
+//Models
+const Violaton = db.define('violation', {
+    isn_dob_bis_extract: STRING,
+    ecb_violation_number: STRING,
+    ecb_violation_status: STRING,
+    dob_violation_number: STRING,
+    bin: STRING,
+    boro: STRING,
+    block: STRING,
+    lot: STRING,
+    hearing_date: STRING,
+    hearing_time: STRING,
+    served_date: STRING,
+    issue_date: STRING,
+    severity: STRING,
+    violation_type: STRING,
+    respondent_name: STRING,
+    respondent_house_number: STRING,
+    respondent_street: STRING,
+    respondent_city: STRING,
+    respondent_zip: STRING,
+    violation_description: STRING,
+    penality_imposed: STRING,
+    amount_paid: STRING,
+    balance_due: STRING,
+    infraction_code1: STRING,
+    section_law_description1: STRING,
+    aggravated_level: STRING,
+    hearing_status: STRING,
+    certification_status: STRING
+})
