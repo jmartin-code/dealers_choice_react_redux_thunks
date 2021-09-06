939241839daa88ae2ea70da0feb8c0394e6a08a0
jmartin-code <jmmartinez331@gmail.com>
2021-09-06 11:51:36 -0400

    wrapped app in provider store and created blank store file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from "./App";
 
-render(<App />, document.querySelector('#root'));
+render(<Provider store={store}> <App /> </Provider>, document.querySelector('#root'));
